0771ba1d4e966e318bf811c18538da9663e05552
Flor3ntEag1e <comba.flo@hotmail.fr>
2019-05-30 17:40:59 +0200

    Ajout projet notepad init

---
+++ app/src/main/java/android/project/notepad/MainActivity.kt
@@ -0,0 +1,12 @@
+package android.project.notepad
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
